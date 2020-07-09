64bc445658c4d66c4e3997ae7d8bae6b6fc0ebd4
sjordan <sjordan@integralads.com>
2020-07-09 15:30:56 -0400

    fixed logins, again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,24 @@
 class UsersController < ApplicationController
 #redirect_if_not_logged_in
-#skip_before_action :redirect_if_not_logged_in, only: [:new, :create]
+#skip_before_action :verified_user, only: [:new, :create]
 
   def new
     @user = User.new
   end
 
   def create
-    if (user = User.new(user_params))
-      session[:user_id] = user.id
-      redirect_to user_path(user)
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
-      render :new
+      render 'new'
     end
   end
 
   def show
-  redirect_if_not_logged_in
+  #redirect_if_not_logged_in
     @user = User.find_by(id: params[:id])
   #  redirect_to '/' if !@user
   end
