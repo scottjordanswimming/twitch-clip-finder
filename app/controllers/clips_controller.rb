f3d7ff84eedba10cf1846ab5e353f5791db7f4fa
sjordan <sjordan@integralads.com>
2020-07-27 17:07:37 -0400

    worked on index filter

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -1,57 +1,50 @@
 class ClipsController < ApplicationController
-
-
-before_action :redirect_if_not_logged_in
-skip_before_action :redirect_if_not_logged_in, only: [:index]
+  before_action :redirect_if_not_logged_in
+  skip_before_action :redirect_if_not_logged_in, only: [:index]
 
   def index
-@clips = Clip.all
+    @clips = Clip.all
   end
 
   def show
-@clip = Clip.find(params[:id])
+    @clip = Clip.find(params[:id])
   end
 
   def new
-@clip = Clip.new
-end
+    @clip = Clip.new
+  end
 
   def create
-#render plain: params[:clip].inspect
-
-@clip = current_user.clips.build(clip_params)
-
-if(@clip.save)
-redirect_to @clip
-else
-  render 'new'
-end
+    #render plain: params[:clip].inspect
+    @clip = current_user.clips.build(clip_params)
+    if(@clip.save)
+      redirect_to @clip
+    else
+      render 'new'
+    end
   end
 
   def edit
-@clip = Clip.find(params[:id])
+    @clip = Clip.find(params[:id])
   end
 
   def update
-@clip = Clip.find(params[:id])
-if(@clip.update(clip_params))
-redirect_to @clip
-else
-  render 'edit'
-end
+    @clip = Clip.find(params[:id])
+    if(@clip.update(clip_params))
+      redirect_to @clip
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-@clip = Clip.find(params[:id])
-@clip.destroy
-redirect_to clips_path
+    @clip = Clip.find(params[:id])
+    @clip.destroy
+    redirect_to clips_path
   end
 
-
-
-
-
-  private def clip_params
-params.require(:clip).permit(:title, :url, :streamer_id, :streamer_ids => [], :streamers_attributes => [:name, :favorite])
-  end
+    private
+    def clip_params
+      params.require(:clip).permit(:title, :url, :streamer_id, :streamer_ids => [], :streamers_attributes => [:name, :favorite])
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,17 @@ class User < ApplicationRecord
     has_many :reviewed_clips, through: :reviews, source: :clip
   has_many :clips
 
+  has_many :streamer_users
+    has_many :streamers, through: :streamer_users
+
+
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true
 
   def favorite_streamers
-    streamers + favorite_streamres
+    streamers + favorite_streamers
   end
 
   def self.create_by_github_omniauth(auth)
